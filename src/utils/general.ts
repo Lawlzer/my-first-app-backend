01479e4aa358eabcdfba58510513a89d7ebac0a4
Lawlzer <Kevindaspam@gmail.com>
2022-08-31 15:36:14 -0500

    ⚰️Remove json-stable-stringify

--- src/utils/general.ts
+++
@@ -1,6 +0,0 @@
-import jsonStableStringify from 'json-stable-stringify';
-
-// If we need the order of a JSON.stringify to be the same, this will guarantee the order
-export function stringifyOrderGuaranteed(input: object) {
-	return jsonStableStringify(input);
-}
